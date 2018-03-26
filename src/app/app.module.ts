6eee4acf80a60a1d552f112bcd84658736bfc242
Ylrah <harly.wcs@gmail.com>
2018-03-26 13:23:46 +0200

    debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+
   ],
   imports: [
     BrowserModule
