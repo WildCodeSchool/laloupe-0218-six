723790375dab278af0b4028eab2b8feda5e44819
v.malleron <v.malleron@gmail.com>
2018-04-10 17:55:29 +0200

    pawns created

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -16,57 +16,74 @@ export class GameComponent implements OnInit {
 
   ngOnInit() {
 
-    window.onload = function () {
+    const canvas = document.getElementById('mycanvas').getContext('2d'), side = 0,
+      size = 30,
+      x = 30,
+      y = 30;
 
-      const canvas = <HTMLCanvasElement>document.getElementById('mycanvas');
+    canvas.beginPath();
+    canvas.moveTo(x + size * Math.cos(0), y + size * Math.sin(0));
 
-      if (!canvas) {
+    for (side; side < 7; side++) {
+      canvas.lineTo(x + size * Math.cos(side * 2 * Math.PI / 6), y + size * Math.sin(side * 2 * Math.PI / 6));
+    }
 
-        alert('Impossible de récupérer le canvas');
-
-        return;
-
-      }
-
-
-      const context = canvas.getContext('2d');
-
-      if (!context) {
-
-        alert('Impossible de récupérer le context du canvas');
-
-        return;
-
-      }
-
-
-
-     // C'est ici que l'on placera tout le code servant à nos dessins.
-     // On n'oublie pas de récupérer le canvas et son context.
-
-
-      context.beginPath();// On démarre un nouveau tracé
-
-      context.moveTo(0, 0);// On se déplace au coin inférieur gauche
-
-      context.lineTo(300, 0);
-
-      context.lineTo(300, 300);
-
-      context.lineTo(0, 300);
-
-      context.stroke();// On trace seulement les lignes.
-
-      context.closePath();
-
-      context.strokeStyle = 'rgba(255, 255, 255)'; // Toutes les prochaines formes 
-      /* pleines seront rouges. */
-
-      context.strokeStyle = 'rgba(255, 255, 255)'; // Toutes les prochaines formes "stroke" 
-      /* seront bleues et semi-transparentes.  */
-    };
+    canvas.fillStyle = "#333333";
+    canvas.fill();
 
   }
-}
+  /* 
+      window.onload = function () {
+  
+        const canvas = <HTMLCanvasElement>document.getElementById('mycanvas');
+  
+        if (!canvas) {
+  
+          alert('Impossible de récupérer le canvas');
+  
+          return;
+  
+        }
+  
+  
+        const context = canvas.getContext('2d');
+  
+        if (!context) {
+  
+          alert('Impossible de récupérer le context du canvas');
+  
+          return; */
+
+
+
+
+
+  // C'est ici que l'on placera tout le code servant à nos dessins.
+  // On n'oublie pas de récupérer le canvas et son context.
+
+  /* 
+        context.beginPath();// On démarre un nouveau tracé
+  
+        context.moveTo(0, 0);// On se déplace au coin inférieur gauche
+  
+        context.lineTo(300, 0);
+  
+        context.lineTo(300, 300);
+  
+        context.lineTo(0, 300);
+  
+        context.stroke();// On trace seulement les lignes.
+  
+        context.closePath(); */
+
+  /* context.strokeStyle = rgb(34, 23, 4);  */// Toutes les prochaines formes 
+  /* pleines seront X. */
+
+  /*    context.fillStyle = 'rgb(112, 58, 255)'; // Toutes les prochaines formes "stroke" 
+     /* seront bleues et semi-transparentes.  */
+};
+
+
+
 
 
