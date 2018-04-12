7ce684422a2e3fdff131b7b0aae844632e315401
v.malleron <v.malleron@gmail.com>
2018-04-12 16:51:34 +0200

    grid created with pixi.js  and Martin

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -17,29 +17,35 @@ import * as PIXI from 'pixi.js';
 })
 
 export class GameComponent implements OnInit {
-  @ViewChild('gridgame')
-  private gridGame: ElementRef;
-  constructor(private renderer: Renderer2) { }
+  app;
+  constructor() { }
 
   ngOnInit() {
-    const app = new PIXI.Application({ transparent: true });
+    this.app = new PIXI.Application({ transparent: true });
     const graphics = new PIXI.Graphics();
-
-    const hex = extendHex({ size: 40 });
-    const grid = defineGrid(hex);
+    const graphicsBlue = new PIXI.Graphics();
+    const hexes = extendHex({ size: 40 });
+    const grid = defineGrid(hexes);
     /* 
         document.body.appendChild(app.view); */
-    this.renderer.appendChild(this.gridGame, app.view);
+    // this.renderer.appendChild(this.gridGame, app.view);
+    document.querySelector('#gridGame').appendChild(this.app.view);
     // set a line style of 1px wide and color #999
-    graphics.lineStyle(1, 0x999999);
-
+    graphics.lineStyle(4, 0xFF0000);
+    graphicsBlue.lineStyle(4, 0x00FF00);
     // render 10,000 hexes
+
     grid.rectangle({ width: 10, height: 10 }).forEach((hex) => {
       const point = hex.toPoint();
+      console.log(hex);
+
+
       // add the hex's position to each of its corner points
       const corners = hex.corners().map(corner => corner.add(point));
       // separate the first from the other corners
       const [firstCorner, ...otherCorners] = corners;
+
+      graphics.lineStyle(4, 0xFFFFFF);
 
       // move the "pen" to the first corner
       graphics.moveTo(firstCorner.x, firstCorner.y);
@@ -48,15 +54,38 @@ export class GameComponent implements OnInit {
       // finish at the first corner
       graphics.lineTo(firstCorner.x, firstCorner.y);
 
-      app.stage.addChild(graphics);
+      this.app.stage.addChild(graphics);
+
+
+
     });
+    grid.rectangle({ width: 10, height: 10 }).forEach((hex) => {
+      const point = hex.toPoint();
+      console.log(hex);
 
 
+      // add the hex's position to each of its corner points
+      const corners = hex.corners().map(corner => corner.add(point));
+      // separate the first from the other corners
+      const [firstCorner, ...otherCorners] = corners;
 
 
+      if (hex.x === 1 && hex.y === 1) {
 
+        /*  graphics.lineStyle(4, 0x00FF00); */
+        graphics.beginFill(0x00FF00);
 
+        const cornersArray = [];
+        corners.forEach(({ x, y }) => cornersArray.push(x, y));
+        graphics.drawPolygon(cornersArray);
 
+        this.app.stage.addChild(graphics);
 
+      }
+
+    });
+  }
+  ngOnDestroy() {
+    document.querySelector('#gridGame').removeChild(this.app.view);
   }
 }
